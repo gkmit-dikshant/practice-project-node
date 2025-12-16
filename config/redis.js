4b1128fbc3203ce69f3b5cdc6502fabf75e9a377
gkmit-dikshant <dikshant@gkmit.co>
2025-12-16 11:12:33 +0530

    fix: update start command for compose file

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,9 @@
 const redis = require("redis");
-const client = redis.createClient();
+const client = redis.createClient({ url: process.env.REDIS_URL });
 
 client
   .connect()
-  .then(() => console.log("redis connected successfully"))
+  .then(() => console.log("redis connected successfully.."))
   .catch((er) => console.error(er.message));
 
 module.exports = {
